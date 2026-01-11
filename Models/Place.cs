558f88f78286cb21e75490f049af04eda5373bb2
Iago Nobre <iagonobre22@gmail.com>
2026-01-11 19:27:51 +0100

    feat: add models

---
+++ Models/Place.cs
@@ -0,0 +1,11 @@
+namespace Explorer.Models;
+
+public class Place
+{
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
---
+++ Models/PlaceCategory.cs
@@ -0,0 +1,7 @@
+namespace Explorer.Models;
+
+public class PlaceCategory
+{
+    public string Key { get; set; } = string.Empty;    
+    public string DisplayName { get; set; } = string.Empty; 
+}
